51dd7c0c96bf2f2e12b0e7dd3cbedf84c3ce7d12
grpandurangi <grpandurangi@gmail.com>
2016-11-15 09:49:54 -0500

    updated

--- roles/mongodbrole_symphony.rb
+++ roles/mongodbrole_symphony.rb
@@ -2,4 +2,14 @@ name "mongodbrole_symphony"
 description "An example Chef role Symphony"
 run_list "recipe[split_mongodb_json]"
 override_attributes({
+   "split_mongodb_json" => {
+    "application"  => "symphony",
+    "ip_address"   => "13.71.151.57",
+    "database"     => "journaldev",
+    "collection"   => "dataset",
+    "username"     => "qaadmin",
+    "password"     => "password",
+    "max_size_in_mb"     => "4"
+   }
+
 })
